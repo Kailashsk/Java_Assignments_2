53f1922c5003ea3e252d9ce3bfbb66f4d2e6fde9
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:36:15 +0530

    Create Q12.java

---
+++ Q12.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class diss1{
+
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Input a number:");
+                int n = sc.nextInt();
+                System.out.println("Number=" + n + "\t" + "Square of number=" + (n*n));
+        } 
+
+}
