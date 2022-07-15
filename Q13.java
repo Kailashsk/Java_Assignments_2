08f30e8085d04cbeaf12e3b7fa69327d466e0455
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:36:38 +0530

    Create Q13.java

---
+++ Q13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class diss1{
+
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Input age:");
+                int a = sc.nextInt();
+                System.out.println("Input height(ft):");
+                int ft = sc.nextInt();
+                System.out.println("Input height(inch):");
+                int inch = sc.nextInt();
+                System.out.println("Input weight:");
+                int w = sc.nextInt();
+                System.out.println("So, you're " + a + "years old, " + ft + "'" + inch + "\"" + " tall and " + w + "KG heavy.");
+        } 
+
+}
